3b02e7539d928f1aab80ad33cecef395d3c2179a
Harendra Barot <shambudanb@gmail.com>
2024-11-05 15:08:51 +0530

    Create Hello.sh
    This is basic Hello program. Looking forward to start my scripting journey for a devOps journey.

---
+++ Control-structure/Hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is script for DevOps Enginnering
+
+echo "DevOps: Hello Friends"
+
+echo "Learning: A guy who wants to teach DevOps , we will comment on this file"
+
+echo "DeOps:Just start the DevOps game"
